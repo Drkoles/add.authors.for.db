978a7b09e4c6c6cd0215a176a469f2f9ae1fea5e
Alexey <a.koles1994@mail.ru>
2015-01-21 15:35:50 +0500

    Add_form
    option

--- add.authors.for.db.php
+++ add.authors.for.db.php
@@ -7,4 +7,23 @@ Version: 1.0.0
 Author: Kolesov Alexey
 Author URI: http://
 */
+add_action('admin_menu', 'CreateMyPluginMenu');
+	function CreateMyPluginMenu()
+	{
+		if (function_exists('add_options_page'))
+		{
+			add_options_page('Настройки Authors.', 'AuthorsSetting', 'manage_options', 'Myplugin', 'MyPluginPageOptions');
+		}
+	}
+	function MyPluginPageOptions()
+	{
+		echo "<h2>Настройки Authors.</h2>";
+?>
+	<form method = "POST">
+	Добавить в базу:
+		<input type = "text" value = "Ссылка на CSV" name = "ssilka">
+		<input type = "submit" name = "Enter">
+	</form>
+<?php
+	}
 ?>
