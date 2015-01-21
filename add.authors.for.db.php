d49efc74bd45f83ae9239d5d2d43dc06cabf3a51
Alexey <a.koles1994@mail.ru>
2015-01-21 22:21:00 +0500

    parscsv

--- add.authors.for.db.php
+++ add.authors.for.db.php
@@ -21,19 +21,16 @@ add_action('admin_menu', 'CreateMyPluginMenu');
 ?>
 	<form id = "ajaxform" method = "POST">
 	ЧТо то там:
-		<input type = "text" value = "" name = "ssilka">
+		<input id = "ssilka" type = "text">
 		<input id = "getForecast" type = "submit" name = "Enter">
 	</form>
-<?php 
-}
-?>
 <script type="text/javascript" >
 (function($) {
 	$(document).on('click', '#getForecast', function(e){
 	e.preventDefault();
 		var data = {
 		'action': 'my_action',
-		'whatever': 'Соси' 
+		'whatever':  $('#ssilka').val()  
 		};
 $.post(ajaxurl, data, function(response) {
 	alert(response);
@@ -42,11 +39,32 @@ $.post(ajaxurl, data, function(response) {
 })(jQuery);
 </script>
 <?php
+}
 add_action( 'wp_ajax_my_action', 'my_action_callback' );
 function my_action_callback() {
 	global $wpdb; 
-	$whatever = $_POST['whatever'];
-        echo $whatever;
-	wp_die(); // this is required to terminate immediately and return a proper response
+	$file_name = $_POST['whatever'];
+	$upload_dir = wp_upload_dir();
+	$base_dir = $upload_dir['path']; // /home/k/kamaev/wp-kama.ru/public_html/wp-content/uploads/2015/01
+	$file_dir = $base_dir."/".$file_name;
+			if(($handle_o = fopen($file_dir , "r")) !== FALSE)
+			{
+				$insertValuesMass = array();
+					while (($data_o = fgetcsv($handle_o, 500, ";")) !== FALSE) 
+					{
+						$insertValues = array();
+						  foreach( $data_o as $v ) 
+						  {
+							$insertValues[] = addslashes(trim($v));
+						  }
+						$year = substr("'".$insertValues[0]."'", 1, 2);		
+						$month = substr("'".$insertValues[0]."'", 3, 2);
+						$page = substr("'".$insertValues[0]."'", 5,-1);
+						$insertValuesMass[] = array($year,$month,$page,$insertValues[1],$insertValues[2],$insertValues[3],$insertValues[4],$insertValues[5]);
+					}
+			}		
+	$whatever = var_dump($insertValuesMass);
+    echo $whatever;
+	wp_die();
 }
 ?>
